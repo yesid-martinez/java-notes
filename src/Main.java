983512dd6a001b1853fbc489345c13ffd5b41118
Yesid M <wujibifan.hh@gmail.com>
2024-12-23 16:26:47 -0500

    equals method & switch-case examples

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        String name = "Yesid";
+        String name = "Axel";
         byte age = 20;
-        int balance = 50000000;
 
-        // format() method create a formatted string
-        // The resulting string can be assigned to a variable;
-        String formattedText = String.format("Your balance is %,d", balance);
+        if (name.equals("Admin") || name.equals( "admin")) {
+            System.out.printf("Usuario incorrecto %n");
+        } else if (age >= 18 && age <= 24) {
+            System.out.printf("Usuario permitido %n");
+        }
 
-        System.out.println(formattedText);
-
-        //Text block (Java 12-13)
-        System.out.println("""
-               Hello %s, you have %d years old and your current balance is $%,d 
-                """.formatted(name, age, balance));
-        // The `formatted` method is a newer method introduced in Java 13 that provides the same functionality
-        // as `format`, but returns a new formatted string directly, instead of requiring an explicit variable.
-        // It can be used directly in method calls, making the code more concise.
-
-        // Key format specifiers
-        // %d, %f, %s %c, %b, %n...
-        // %+d, %-d, %,d, %.2f...
-
-        Casting.implicitConversionExamples();
-        Casting.explicitConversionExamples();
+        switch (name) {
+            case "Yesid":
+                System.out.print("Bienvenido Yesid");
+                break;
+            case "Axel":
+                System.out.print("Bienvenido Axel");
+                break;
+            default:
+                System.out.print("Solo personal autorizado.");
+        }
     }
 }
