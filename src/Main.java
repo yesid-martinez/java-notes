8d7c1f4a9074b726ac9a035f7a0f002b52b89fa7
Yesid M <wujibifan.hh@gmail.com>
2024-12-27 20:25:11 -0500

    input handling with Scanner class and exception management

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,42 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class Main {
     public static void main(String[] args) {
-        String name = "Axel";
-        byte age = 20;
 
-        if (name.equals("Admin") || name.equals( "admin")) {
-            System.out.printf("Usuario incorrecto %n");
-        } else if (age >= 18 && age <= 24) {
-            System.out.printf("Usuario permitido %n");
+        /* The Scanner class is used to read input data
+         This data can be read from various input sources: files, input streams, strings and user keyboard inputs...*/
+        Scanner input = new Scanner(System.in);
+        // Takes as its source the standard input stream of the system (Keyboard)
+
+        System.out.printf("Ingrese el nombre de su película favorita: %n");
+        String movie = input.nextLine();
+        // nextLine(): Reads a complete line of text (until Enter is pressed).
+
+        System.out.printf(movie + " movie is all we need on a dark night: %n");
+
+        System.out.printf("Indíca cuántas veces la has visto: %n");
+
+        int times;
+
+        try {
+            times = input.nextInt();
+
+            if (times >= 3) {
+                System.out.print("That's amazing, you'r a big fan dude!");
+                // It is good practice to close the Scanner object when it is no longer needed to free up resources
+            }
+
+            if (0 < times && times < 3) {
+                System.out.println("Okay, that's good!");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Por favor, ingresa un número válido.");
+        } finally {
+            input.close();
         }
 
-        switch (name) {
-            case "Yesid":
-                System.out.print("Bienvenido Yesid");
-                break;
-            case "Axel":
-                System.out.print("Bienvenido Axel");
-                break;
-            default:
-                System.out.print("Solo personal autorizado.");
-        }
+        /*next(): Reads the next token (a word or text separated by spaces)
+        hasNext(), hasNextInt(), etc..: Checks if more input of a specific type is available*/
     }
 }
